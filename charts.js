d5f2f6393d92649bda149b21f52c49a70ae22125
margomerritt <missmargomerritt@gmail.com>
2023-01-05 14:48:27 -0500

    Updating analysis

--- charts.js
+++ charts.js
@@ -68,10 +68,16 @@ function buildCharts(sample) {
 
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
     
+    var sampleNumber = data.sampleNumber;
+
+    var filteredArray = sampleNumber.filter(sampleObj => sampleObj.id == sample);
+
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
     var firstSample = filteredSamples[0];
 
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
+
+    var firstSampleArray = filteredArray[0];
 
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var id = firstSample.otu_ids;
@@ -82,6 +88,9 @@ function buildCharts(sample) {
 
     // Deliverable 3: 3. Create a variable that holds the washing frequency.
 
+    var washingFrequency = firstSampleArray.wfreq;
+
+    var washingFrequencyFloat = parseFloat(washingFrequency); 
 
     // Deliverable 1: 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
@@ -134,9 +143,32 @@ function buildCharts(sample) {
 
     // Deliverable 3: 4. Create the trace for the gauge chart.
     
+    var gaugeData = [
+      {
+      value: washingFrequencyFloat,
+      type: "indicator",
+      mode: "gauge+number",
+      title: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week",
+      gauge: {
+        axis: { range: [null, 10], tickwidth: 1, tickcolor: "black"},
+        bar: {color: "black"},
+        steps: [
+          {range: [0, 2], color: "red"},
+          {range: [2, 4], color: "darkorange"},
+          {range: [4, 6], color: "yellow"},
+          {range: [6, 8], color: "limegreen"},
+          {range: [8, 10], color: "darkgreen"}
+        ],
+    }
+  }
+];
+
+
     // Deliverable 3: 5. Create the layout for the gauge chart.
-
+    var gaugeLayout = {
+      automargin: true
+    };
     // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
-
+      Plotly.newPlot("gauge", gaugeData, gaugeLayout)
   });
 }
